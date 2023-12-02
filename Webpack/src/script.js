9fcc211de91df81ae1643cdfccfb98b10ecc15b9
cod3kid <djsufail@gmail.com>
2023-12-02 10:34:44 +0530

    ThreeJS: Gsap to() method

--- Webpack/src/script.js
+++ Webpack/src/script.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
+import gsap from "gsap";
 
 const scene = new THREE.Scene();
 
@@ -8,6 +9,8 @@ const material = new THREE.MeshBasicMaterial({ color: "pink" });
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
+
+gsap.to(mesh.position, { duration: 2, delay: 1, x: 2 });
 
 const aspect = {
   width: window.innerWidth,
@@ -31,13 +34,13 @@ const clock = new THREE.Clock();
 const animate = () => {
   // GetElapsedTime
   const elapsedTime = clock.getElapsedTime();
-  mesh.rotation.y = elapsedTime * (Math.PI / 2);
+  //   mesh.rotation.y = elapsedTime * (Math.PI / 2);
   // Linear Function
   //   mesh.position.x = elapsedTime;
   //   mesh.position.y = elapsedTime;
 
-  mesh.position.x = Math.sin(elapsedTime);
-  mesh.position.y = Math.cos(elapsedTime);
+  //   mesh.position.x = Math.sin(elapsedTime);
+  //   mesh.position.y = Math.cos(elapsedTime);
   //   mesh.position.x = Math.tan(elapsedTime);
 
   renderer.render(scene, camera);
